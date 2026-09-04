53dc1ae5a787efffda52a7e844f459e21011db09
contaempresa243-eng <contaempresa243@gmail.com>
2026-09-04 12:17:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,44 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('farmastock-cache-v1').then((cache) => {
-            return cache.addAll([
-                './index.html'
-            ]);
-        })
-    );
+const CACHE_NAME = 'farmastock-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instala o Service Worker e guarda os ficheiros em cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+// Ativa e limpa caches antigas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepa os pedidos para carregar instantaneamente da cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Retorna da cache se existir, senão vai à rede
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
